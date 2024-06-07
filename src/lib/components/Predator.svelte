27e2283760d05b170a776e32c58e808a2fa6dd1e
Alex Casals <alexcasals95@gmail.com>
2024-06-07 19:58:30 +0200

    feat: Improve predator movement and behavior
    The code changes in this commit improve the movement and behavior of the predator component. It introduces a new logic to calculate the initial direction and distance threshold for the predator's movement towards the player fish. Additionally, it adds a check to remove the predator from the scene if it goes beyond a certain distance threshold. This enhancement enhances the overall functionality and realism of the predator behavior in the scene.

--- src/lib/components/Predator.svelte
+++ src/lib/components/Predator.svelte
@@ -2,12 +2,18 @@
 	import { playerPosition } from '$lib/stores/store';
 	import { T, useTask } from '@threlte/core';
 	import { Float } from '@threlte/extras';
-	import { onMount } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 	import { get } from 'svelte/store';
-	import type { Mesh } from 'three';
+	import { Mesh, Vector3 } from 'three';
 
-	let position = [0, 0, 0];
+	const dispatcher = createEventDispatcher();
+
 	let mesh: Mesh;
+	let position = [0, 0, 0];
+	let distanceThreshold = 20;
+	let direction = new Vector3();
+	let initialDirectionSet = false;
+	let hasPassedPlayerFish = false;
 
 	onMount(() => {
 		generateRandomPosition();
@@ -15,10 +21,39 @@
 
 	useTask((delta) => {
 		if (!mesh) return;
+
 		const targetPosition = get(playerPosition);
 		const currentPosition = mesh.position;
-		currentPosition.lerp(targetPosition, delta * 0.1);
+
+		// Calcular la dirección inicial si no se ha calculado antes
+		if (!initialDirectionSet) {
+			direction = new Vector3().subVectors(targetPosition, currentPosition).normalize();
+			initialDirectionSet = true;
+		}
+
+		// Verificar la distancia al PlayerFish
+		const distance = currentPosition.distanceTo(targetPosition);
+
+		// Si la distancia es menor a un umbral pequeño, continuar en la dirección original sin recalcular
+		if (distance < 1) {
+			hasPassedPlayerFish = true;
+		}
+
+		// Si aún no ha pasado el PlayerFish, recalcular la dirección
+		if (!hasPassedPlayerFish) {
+			direction = new Vector3().subVectors(targetPosition, currentPosition).normalize();
+		}
+
+		// Mover al depredador en la dirección del PlayerFish o en la misma dirección si ya lo ha pasado
+		currentPosition.add(direction.clone().multiplyScalar(delta * 5));
+
+		// Copiar la nueva posición al mesh
 		mesh.position.copy(currentPosition);
+
+		// Verificar si el depredador está fuera del campo de visión
+		if (currentPosition.distanceTo(targetPosition) > distanceThreshold) {
+			dispatcher('remove');
+		}
 	});
 
 	function generateRandomPosition(): void {
