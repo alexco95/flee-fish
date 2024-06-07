b2a60f5d2f374ebdd2d15145e0d32b7998b9a0e4
Alex Casals <alexcasals95@gmail.com>
2024-06-07 20:54:42 +0200

    feat: Add keyboard controls for player fish movement

--- src/lib/components/PlayerFish.svelte
+++ src/lib/components/PlayerFish.svelte
@@ -1,16 +1,80 @@
 <script lang="ts">
 	import { playerPosition } from '$lib/stores/store';
-	import { T } from '@threlte/core';
+	import { T, useTask } from '@threlte/core';
 	import { Float } from '@threlte/extras';
 	import type { Mesh } from 'three';
 	import ThirdPersonControls from './ThirdPersonControls.svelte';
+	import { onMount } from 'svelte';
 
 	let player: Mesh;
 	let playerRef: Mesh;
 
+	const boundaries = {
+		xMin: -5,
+		xMax: 5,
+		yMin: -5,
+		yMax: 5
+	};
+
+	const speed = 10;
+
+	const keysPressed = {
+		ArrowUp: false,
+		ArrowDown: false,
+		ArrowLeft: false,
+		ArrowRight: false,
+		KeyW: false,
+		KeyA: false,
+		KeyS: false,
+		KeyD: false
+	};
+
 	$: if (player) {
 		playerPosition.set(player.position);
 		playerRef = player;
+	}
+
+	onMount(() => {
+		window.addEventListener('keydown', handleKeyDown);
+		window.addEventListener('keyup', handleKeyUp);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+			window.removeEventListener('keyup', handleKeyUp);
+		};
+	});
+
+	useTask((delta) => {
+		if (player) {
+			const position = player.position;
+
+			if (keysPressed.ArrowUp || keysPressed.KeyW) {
+				position.y = Math.min(position.y + delta * speed, boundaries.yMax);
+			}
+			if (keysPressed.ArrowDown || keysPressed.KeyS) {
+				position.y = Math.max(position.y - delta * speed, boundaries.yMin);
+			}
+			if (keysPressed.ArrowLeft || keysPressed.KeyA) {
+				position.x = Math.max(position.x + delta * speed, boundaries.xMin);
+			}
+			if (keysPressed.ArrowRight || keysPressed.KeyD) {
+				position.x = Math.min(position.x - delta * speed, boundaries.xMax);
+			}
+
+			playerPosition.set(player.position);
+		}
+	});
+
+	function handleKeyDown(event: KeyboardEvent) {
+		if (keysPressed.hasOwnProperty(event.code)) {
+			keysPressed[event.code] = true;
+		}
+	}
+
+	function handleKeyUp(event: KeyboardEvent) {
+		if (keysPressed.hasOwnProperty(event.code)) {
+			keysPressed[event.code] = false;
+		}
 	}
 </script>
 
