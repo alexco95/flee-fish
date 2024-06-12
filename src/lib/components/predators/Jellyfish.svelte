eda45ead857f8b28843ca3171d268a80969639c4
Alex Casals <alexcasals95@gmail.com>
2024-06-12 20:28:45 +0200

    refactor: Improve predator movement logic and collision handling

--- src/lib/components/predators/Jellyfish.svelte
+++ src/lib/components/predators/Jellyfish.svelte
@@ -1,25 +1,32 @@
 <script lang="ts">
 	import { T, useTask } from '@threlte/core';
 	import { Float } from '@threlte/extras';
-	import { AutoColliders, RigidBody } from '@threlte/rapier';
-	import { Mesh } from 'three';
+	import { AutoColliders, RigidBody, type CollisionEnterEvent } from '@threlte/rapier';
+	import { Group } from 'three';
 	import { PredatorService } from './predator-service';
+	import RAPIER from '@dimforge/rapier3d-compat';
 
-	let mesh: Mesh;
-	let rigidBody: RigidBody;
+	let jellyFish: Group;
+	let rigidBody: RAPIER.RigidBody;
 	let predatorService: PredatorService;
 
-	$: if (mesh && rigidBody) {
-		predatorService = new PredatorService(mesh, rigidBody);
+	$: if (jellyFish && rigidBody) {
+		predatorService = new PredatorService(jellyFish, rigidBody);
 	}
 
 	useTask((delta) => {
 		predatorService.updateTrajectory(delta);
 	});
+
+	function handleCollision(event: CollisionEnterEvent) {
+		if (event.targetRigidBody) {
+			predatorService.handleCollision(event.targetRigidBody);
+		}
+	}
 </script>
 
-<T.Group bind:ref={mesh}>
-	<RigidBody bind:rigidBody gravityScale={0}>
+<T.Group bind:ref={jellyFish}>
+	<RigidBody bind:rigidBody gravityScale={0} on:collisionenter={handleCollision}>
 		<AutoColliders>
 			<Float speed={10}>
 				<T.Mesh>
--- src/lib/components/predators/predator-service.ts
+++ src/lib/components/predators/predator-service.ts
@@ -1,15 +1,14 @@
 import { reduceHealth } from "$lib/stores/healthStore";
 import { playerPosition } from "$lib/stores/store";
+import type RAPIER from "@dimforge/rapier3d-compat";
 import { get } from "svelte/store";
-import { Mesh, Vector3 } from "three";
+import { Group, Mesh, Vector3 } from "three";
 
 export class PredatorService {
     private direction = new Vector3();
-    private initialDirectionSet = false;
-    private hasPassedPlayerFish = false;
     private hasCollided = false;
 
-    constructor(private mesh: Mesh, private rigidBody: any ) {
+    constructor(private mesh: Group, private rigidBody: RAPIER.RigidBody ) {
         this.mesh.position.copy(this.generateRandomPosition());
         this.rigidBody.setTranslation(this.mesh.position, true);
     }
@@ -21,28 +20,9 @@ export class PredatorService {
         const currentPosition = this.mesh.position;
 
         // Calcular la dirección inicial si no se ha calculado antes
-        if (!this.initialDirectionSet) {
-            this.direction = new Vector3().subVectors(targetPosition, currentPosition).normalize();
-            this.initialDirectionSet = true;
-        }
-
-        // Verificar la distancia al PlayerFish
-        const distance = currentPosition.distanceTo(targetPosition);
-
-        // Si no ha colisionado, sigue persiguiendo al PlayerFish
         if (!this.hasCollided) {
-            // Si la distancia es menor a un umbral pequeño, realizar colisión
-            if (distance < 1) {
-                this.hasCollided = true;
-                reduceHealth(5);
-                // Cambiar dirección aleatoriamente después de la colisión
-                this.direction = new Vector3(Math.random() - 0.5, Math.random() - 0.5, 0).normalize();
-            } else {
-                // Si aún no ha pasado el PlayerFish, recalcular la dirección
-                if (!this.hasPassedPlayerFish) {
-                    this.direction = new Vector3().subVectors(targetPosition, currentPosition).normalize();
-                }
-            }
+            this.direction = new Vector3().subVectors(targetPosition, currentPosition).normalize();
+            // this.initialDirectionSet = true;
         }
 
         // Mover al depredador en la dirección del PlayerFish o en una nueva dirección aleatoria si ha colisionado
@@ -52,6 +32,17 @@ export class PredatorService {
         this.mesh.position.copy(currentPosition);
         this.rigidBody.setTranslation(currentPosition, true);
     }
+
+	handleCollision(targetRigidBody: RAPIER.RigidBody): void {
+		  this.hasCollided = true;
+
+		  // Cambiar dirección aleatoriamente después de la colisión
+		  this.direction = new Vector3(Math.random() - 0.5, Math.random() - 0.5, 0).normalize();
+
+		  if (targetRigidBody.handle === 0) { // consider adding playerFish name into rigid body userData
+			  reduceHealth(5);
+		  }
+	  }
 
     private generateRandomPosition(): Vector3 {
 		const x = Math.random() * 10 - 5;
